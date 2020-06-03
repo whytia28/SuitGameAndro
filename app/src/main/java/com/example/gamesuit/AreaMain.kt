cff22526b45be5bc0e057e12e0e317e460419e29
whytia28 <wahyu.sp9@gmail.com>
2020-06-03 17:25:05 +0700

    Alert dialog

--- app/src/main/java/com/example/gamesuit/AreaMain.kt
+++ app/src/main/java/com/example/gamesuit/AreaMain.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_area_main.*
 
 class AreaMain : AppCompatActivity() {
@@ -32,27 +33,32 @@ class AreaMain : AppCompatActivity() {
         batu1.setOnClickListener {
             pilihanSatu = "batu"
             showResult()
+            batu1.foreground = resources.getDrawable(R.drawable.overlay, null)
         }
         kertas1.setOnClickListener {
             pilihanSatu = "kertas"
             showResult()
+            kertas1.foreground = resources.getDrawable(R.drawable.overlay, null)
         }
         gunting1.setOnClickListener {
             pilihanSatu = "gunting"
             showResult()
+            gunting1.foreground = resources.getDrawable(R.drawable.overlay, null)
         }
         batu2.setOnClickListener {
             pilihanDua = "batu"
             showResult()
+            batu2.foreground = resources.getDrawable(R.drawable.overlay, null)
         }
         kertas2.setOnClickListener {
             pilihanDua = "kertas"
             showResult()
+            kertas2.foreground = resources.getDrawable(R.drawable.overlay, null)
         }
         gunting2.setOnClickListener {
             pilihanDua = "gunting"
             showResult()
-
+            gunting2.foreground = resources.getDrawable(R.drawable.overlay, null)
         }
         main_lagi.setOnClickListener {
             startNew()
@@ -60,33 +66,42 @@ class AreaMain : AppCompatActivity() {
     }
 
     private fun showResult() {
+        var pemenang : String = ""
         if (pilihanSatu != "" && pilihanDua != "") {
             val control = Controler()
             val hasilMain = control.caraMain(pilihanSatu, pilihanDua)
             if (hasilMain == "pemain 1 menang") {
-                hasil.text = "$namaPemain1 MENANG"
+                pemenang = "$namaPemain1 MENANG"
+//                hasil.text = "$namaPemain1 MENANG"
             } else if (hasilMain == "pemain 2 menang") {
-                hasil.text = "$namaPemain2 MENANG"
+                pemenang = "$namaPemain2 MENANG"
+//                hasil.text = "$namaPemain2 MENANG"
             } else {
-                hasil.text = "DRAW"
+                pemenang = "DRAW"
+//                hasil.text = "DRAW"
             }
-            hasil.visibility = View.VISIBLE
-        } else {
-            Toast.makeText(this, "Kedua pemain harus memilih", Toast.LENGTH_SHORT).show()
+//            hasil.visibility = View.VISIBLE
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("HASIL")
+            builder.setMessage("$pemenang")
+            builder.setPositiveButton("OK", null)
+            builder.create().show()
         }
+//        else {
+//            Toast.makeText(this, "Kedua pemain harus memilih", Toast.LENGTH_SHORT).show()
+//        }
 
     }
 
-    fun startNew() {
-        hasil.visibility = View.GONE
+    private fun startNew() {
+//        hasil.visibility = View.GONE
         pilihanSatu = ""
         pilihanDua = ""
+        batu1.foreground = null
+        batu2.foreground = null
+        kertas1.foreground = null
+        kertas2.foreground = null
+        gunting1.foreground = null
+        gunting2.foreground = null
     }
-
-    fun onClick(view: View) {
-        val animation = AnimationUtils.loadAnimation(this, R.anim.fade)
-        batu1.startAnimation(animation)
-    }
-
-
 }
